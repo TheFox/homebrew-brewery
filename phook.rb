49cb02754597a89548432858c04a3950d283dd22
Christian Mayer <christian@fox21.at>
2017-04-12 10:25:06 +0200

    phook 0.0.5

--- phook.rb
+++ phook.rb
@@ -2,8 +2,8 @@
 class Phook < Formula
   desc "Runs commands around a parent process"
   homepage "https://github.com/drinchev/phook"
-  url "https://github.com/drinchev/phook/archive/v0.0.4.tar.gz"
-  sha256 "f2b633721048a2681cb4613b57af08269cdb8e54d70cfa77abb1cdc94cc7b562"
+  url "https://github.com/drinchev/phook/archive/v0.0.5.tar.gz"
+  sha256 "eb6c81845f7b9f535e1ea36abfde0f9c8f59fe7d342eecf4602c9f6cfd57f0d4"
 
   depends_on "make" => :build
 
