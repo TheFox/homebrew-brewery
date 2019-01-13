11a2b4e3f79218fdb9820d997d92e85fad47f352
Christian Mayer <christian@fox21.at>
2019-01-13 10:54:38 +0100

    Removed wallet-cpp-debug.

--- bin/release_walletcpp.sh
+++ bin/release_walletcpp.sh
@@ -5,6 +5,3 @@ SCRIPT_BASEDIR=$(dirname "$0")
 cd "${SCRIPT_BASEDIR}/.."
 
 ./bin/release.sh wallet-cpp $@
-
-export GITHUB_REPO_URL=https://github.com/TheFox/wallet-cpp
-./bin/release.sh wallet-cpp-debug $@
