cd7bb1ea9a18365cea7de18373877779ea8cafcf
Christian Mayer <christian@fox21.at>
2019-05-31 23:00:55 +0200

    --HEAD and --with-debug options for Parameters 1.0.0-rc.5.

--- parameters.rb
+++ parameters.rb
@@ -6,18 +6,24 @@
 class Parameters < Formula
   desc "Automatic replace variables in configuration file templates from environment variables."
   homepage "https://github.com/TheFox/parameters"
+  head "https://github.com/TheFox/parameters.git"
   url "https://github.com/TheFox/parameters/archive/v1.0.0-rc.5.tar.gz"
   sha256 "4de47411570e25cc5c29f9d68f628c24aefeeafcc586e51658633bb5f3eaed97"
 
   depends_on "cmake" => :build
 
+  option "with-debug", "Build a debug version"
+
   def install
+    # Build Options
+    build_type = build.with?('debug') ? 'debug' : 'release'
     sha256_short = "4de47411570e25cc5c29f9d68f628c24aefeeafcc586e51658633bb5f3eaed97"[0..7]
+
     args = %W[
-      -DCMAKE_BUILD_TYPE=release
+      -DCMAKE_BUILD_TYPE=#{build_type}
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DPROJECT_VERSION_FULL=1.0.0-rc.5
-      -DPROJECT_VERSION_HASH=fake-#{sha256_short}
+      -DPROJECT_VERSION_HASH=brew-#{sha256_short}
     ]
 
     system "mkdir build"
--- skel/parameters.rb
+++ skel/parameters.rb
@@ -6,18 +6,24 @@
 class Parameters < Formula
   desc "Automatic replace variables in configuration file templates from environment variables."
   homepage "https://github.com/TheFox/parameters"
+  head "https://github.com/TheFox/parameters.git"
   url "https://github.com/TheFox/parameters/archive/%VERSION%.tar.gz"
   sha256 "%SHA256%"
 
   depends_on "cmake" => :build
 
+  option "with-debug", "Build a debug version"
+
   def install
+    # Build Options
+    build_type = build.with?('debug') ? 'debug' : 'release'
     sha256_short = "%SHA256%"[0..7]
+
     args = %W[
-      -DCMAKE_BUILD_TYPE=release
+      -DCMAKE_BUILD_TYPE=#{build_type}
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DPROJECT_VERSION_FULL=%VERSION_SHORT%
-      -DPROJECT_VERSION_HASH=fake-#{sha256_short}
+      -DPROJECT_VERSION_HASH=brew-#{sha256_short}
     ]
 
     system "mkdir build"
