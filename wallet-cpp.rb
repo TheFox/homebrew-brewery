4e31514379fee1abb42e24e898a89b1aef8b0cf8
Christian Mayer <christian@fox21.at>
2019-07-21 10:54:16 +0200

    wallet-cpp 0.9.0

--- wallet-cpp.rb
+++ wallet-cpp.rb
@@ -1,14 +1,14 @@
 
 # File type: production
-# Version: v0.8.4
-# Version Short: 0.8.4
+# Version: v0.9.0
+# Version Short: 0.9.0
 
 class WalletCpp < Formula
   desc "A spreadsheet likewise C++17 program to track your finances."
   homepage "https://github.com/TheFox/wallet-cpp"
   head "https://github.com/TheFox/wallet-cpp.git"
-  url "https://github.com/TheFox/wallet-cpp/archive/v0.8.4.tar.gz"
-  sha256 "9db1217dea67fc3b5ac15f8a06667732a3395b6f94aaa08bc715091288fe9c1e"
+  url "https://github.com/TheFox/wallet-cpp/archive/v0.9.0.tar.gz"
+  sha256 "9e05a13bf281f19857de75bfbc34db31e3bbf3fb4f95dab5bf71c28bcc43d874"
 
   depends_on "cmake" => :build
   depends_on "boost" => ">=1.62"
@@ -21,13 +21,13 @@ class WalletCpp < Formula
   def install
     # Build Options
     build_type = build.with?('debug') ? 'debug' : 'release'
-    sha256_short = "9db1217dea67fc3b5ac15f8a06667732a3395b6f94aaa08bc715091288fe9c1e"[0..7]
+    sha256_short = "9e05a13bf281f19857de75bfbc34db31e3bbf3fb4f95dab5bf71c28bcc43d874"[0..7]
 
     args = %W[
       -DCMAKE_BUILD_TYPE=#{build_type}
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DPROJECT_SHARE_PREFIX=#{share}
-      -DPROJECT_VERSION_FULL=0.8.4
+      -DPROJECT_VERSION_FULL=0.9.0
       -DPROJECT_VERSION_HASH=brew-#{sha256_short}
     ]
 
