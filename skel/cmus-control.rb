040d0a977a62a5626ae56955c967bcbf8e1ec5c5
Christian Mayer <christian@fox21.at>
2022-12-06 18:23:11 +0100

    Remove indent from CmusControl Formula.

--- skel/cmus-control.rb
+++ skel/cmus-control.rb
@@ -16,47 +16,47 @@ class CmusControl < Formula
     bin.install "build/release/bin/cmuscontrold"
   end
 
-  def caveats; <<-EOS.indent
-    Since Cmus Control doesn't have the behavior of changing any foreign processes it's highly recommended to deactivate Apples Remote Control Daemon:
+  def caveats; <<-EOS
+Since Cmus Control doesn't have the behavior of changing any foreign processes it's highly recommended to deactivate Apples Remote Control Daemon:
 
-      launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
+  launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
 
-    See more details about Remote Control Daemon related to Cmus Control in this blog post:
+See more details about Remote Control Daemon related to Cmus Control in this blog post:
 
-      https://blog.fox21.at/2015/11/20/control-cmus-with-media-keys.html
-    EOS
+  https://blog.fox21.at/2015/11/20/control-cmus-with-media-keys.html
+EOS
   end
 
   plist_options :startup => true, :manual => "cmuscontrold"
 
-  def plist; <<-EOS.indent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key> <string>#{plist_name}</string>
+  def plist; <<-EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key> <string>#{plist_name}</string>
 
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/cmuscontrold</string>
-        </array>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{opt_bin}/cmuscontrold</string>
+    </array>
 
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin</string>
-        </dict>
+    <key>EnvironmentVariables</key>
+    <dict>
+      <key>PATH</key>
+      <string>/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin</string>
+    </dict>
 
-        <key>ProcessType</key> <string>Background</string>
+    <key>ProcessType</key> <string>Background</string>
 
-        <key>RunAtLoad</key> <true />
+    <key>RunAtLoad</key> <true />
 
-        <key>KeepAlive</key> <true />
+    <key>KeepAlive</key> <true />
 
-        <key>Disabled</key> <false />
-      </dict>
-    </plist>
-    EOS
+    <key>Disabled</key> <false />
+  </dict>
+</plist>
+EOS
   end
 
   test do
