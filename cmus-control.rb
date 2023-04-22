f123cae71c1c0d807917064cb92b4589431b3d30
Christian Mayer <christian@fox21.at>
2023-04-22 14:43:24 +0200

    Calling plist_options is deprecated. Use service instead.
    Closes #9.

--- cmus-control.rb
+++ cmus-control.rb
@@ -9,6 +9,12 @@ class CmusControl < Formula
   sha256 "86893293aed440eca0bbeaa46358849d1a072cd10f357476dc3ab1c3501e26db"
 
   depends_on "cmake" => :build
+  service do
+    run [opt_bin/"cmuscontrold"]
+    run_type :immediate
+    process_type :background
+    keep_alive true
+  end
 
   def install
     system "./bin/build_release.sh"
@@ -24,38 +30,6 @@ Since Cmus Control doesn't have the behavior of changing any foreign processes i
 See more details about Remote Control Daemon related to Cmus Control in this blog post:
 
   https://blog.fox21.at/2015/11/20/control-cmus-with-media-keys.html
-EOS
-  end
-
-  plist_options :startup => true, :manual => "cmuscontrold"
-
-  def plist; <<-EOS
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key> <string>#{plist_name}</string>
-
-    <key>ProgramArguments</key>
-    <array>
-      <string>#{opt_bin}/cmuscontrold</string>
-    </array>
-
-    <key>EnvironmentVariables</key>
-    <dict>
-      <key>PATH</key>
-      <string>/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin</string>
-    </dict>
-
-    <key>ProcessType</key> <string>Background</string>
-
-    <key>RunAtLoad</key> <true />
-
-    <key>KeepAlive</key> <true />
-
-    <key>Disabled</key> <false />
-  </dict>
-</plist>
 EOS
   end
 
